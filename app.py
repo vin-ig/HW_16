3ae119bedd468860cc02287cc1af1e31b5b96a3d
Harry <vin-ig@yandex.ru>
2022-05-05 21:51:43 +0300

    All methods realized

--- app.py
+++ app.py
@@ -35,12 +35,8 @@ class Order(db.Model):
 	end_date = db.Column(db.Date)
 	address = db.Column(db.String(50))
 	price = db.Column(db.Integer)
-
 	customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	# customer = db.relationship('User')
-
 	executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	# executor = db.relationship('User')
 
 
 class Offer(db.Model):
@@ -91,26 +87,43 @@ with db.session.begin():
 		))
 
 
-@app.route('/users/')
+@app.route('/users/', methods=['GET', 'POST'])
 def users():
-	result = []
-	for user in db.session.query(User).all():
-		result.append({
-			'age': user.age,
-			'email': user.email,
-			'first_name': user.first_name,
-			'id': user.id,
-			'last_name': user.last_name,
-			'phone': user.phone,
-			'role': user.role
-			})
-	return jsonify(result)
+	if request.method == 'GET':
+		result = []
+		for user in db.session.query(User).all():
+			result.append({
+				'age': user.age,
+				'email': user.email,
+				'first_name': user.first_name,
+				'id': user.id,
+				'last_name': user.last_name,
+				'phone': user.phone,
+				'role': user.role
+				})
+		return jsonify(result)
+
+	elif request.method == 'POST':
+		user = request.json
+		new_user = User(
+			age=user.get('age'),
+			email=user.get('email'),
+			first_name=user.get('first_name'),
+			last_name=user.get('last_name'),
+			phone=user.get('phone'),
+			role=user.get('role')
+		)
+		with db.session.begin():
+			db.session.add(new_user)
+		return f'Новый пользователь по имени {new_user.first_name} {new_user.last_name} успешно добавлен в базу!'
 
 
-@app.route('/users/<int:id>/')
+@app.route('/users/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def user_by_id(id):
 	user = db.session.query(User).get(id)
-	return jsonify({
+
+	if request.method == 'GET':
+		return jsonify({
 			'age': user.age,
 			'email': user.email,
 			'first_name': user.first_name,
@@ -118,63 +131,153 @@ def user_by_id(id):
 			'last_name': user.last_name,
 			'phone': user.phone,
 			'role': user.role
-			})
-
-
-@app.route('/orders/')
-def orders():
-	result = []
-	for order in db.session.query(Order).all():
-		result.append({
-			'id': order.id,
-			'name': order.name,
-			'description': order.description,
-			'start_date': order.start_date,
-			'end_date': order.end_date,
-			'address': order.address,
-			'price': order.price,
-			'customer_id': order.customer_id,
-			'executor_id': order.executor_id
 		})
-	return jsonify(result)
+
+	elif request.method == 'PUT':
+		update = request.json
+
+		user.age = update.get('age', user.age)
+		user.email = update.get('email', user.email)
+		user.first_name = update.get('first_name', user.first_name)
+		user.last_name = update.get('last_name', user.last_name)
+		user.phone = update.get('phone', user.phone)
+		user.role = update.get('role', user.role)
+
+		db.session.add(user)
+		db.session.commit()
+		return f'Данные пользователя {user.first_name} {user.last_name} с ID {user.id} успешно обновлены!'
+
+	elif request.method == 'DELETE':
+		db.session.delete(user)
+		db.session.commit()
+		return f'Пользователь {user.first_name} {user.last_name} с ID {user.id} успешно удален из базы!'
 
 
-@app.route('/orders/<int:id>/')
+@app.route('/orders/', methods=['GET', 'POST'])
+def orders():
+	if request.method == 'GET':
+		result = []
+		for order in db.session.query(Order).all():
+			result.append({
+				'id': order.id,
+				'name': order.name,
+				'description': order.description,
+				'start_date': order.start_date,
+				'end_date': order.end_date,
+				'address': order.address,
+				'price': order.price,
+				'customer_id': order.customer_id,
+				'executor_id': order.executor_id
+			})
+		return jsonify(result)
+
+	elif request.method == 'POST':
+		order = request.json
+		new_order = Order(
+			id=order.get('id'),
+			name=order.get('name'),
+			description=order.get('description'),
+			start_date=datetime.strptime(order.get('start_date'), '%m/%d/%Y').date(),
+			end_date=datetime.strptime(order.get('end_date'), '%m/%d/%Y').date(),
+			address=order.get('address'),
+			price=order.get('price'),
+			customer_id=order.get('customer_id'),
+			executor_id=order.get('executor_id')
+		)
+		with db.session.begin():
+			db.session.add(new_order)
+		return f'Новый заказ "{new_order.name}" успешно добавлен в базу!'
+
+
+@app.route('/orders/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def order_by_id(id):
 	order = db.session.query(Order).get(id)
-	return jsonify({
-			'id': order.id,
-			'name': order.name,
-			'description': order.description,
-			'start_date': order.start_date,
-			'end_date': order.end_date,
-			'address': order.address,
-			'price': order.price,
-			'customer_id': order.customer_id,
-			'executor_id': order.executor_id
-			})
+
+	if request.method == 'GET':
+		return jsonify({
+				'id': order.id,
+				'name': order.name,
+				'description': order.description,
+				'start_date': order.start_date,
+				'end_date': order.end_date,
+				'address': order.address,
+				'price': order.price,
+				'customer_id': order.customer_id,
+				'executor_id': order.executor_id
+				})
+
+	elif request.method == 'PUT':
+		update = request.json
+
+		order.name = update.get('name', order.name)
+		order.description = update.get('description', order.description)
+		order.start_date = update.get('start_date', order.start_date)
+		order.end_date = update.get('end_date', order.end_date)
+		order.address = update.get('address', order.address)
+		order.price = update.get('price', order.price)
+		order.customer_id = update.get('customer_id', order.customer_id)
+		order.executor_id = update.get('executor_id', order.executor_id)
+
+		db.session.add(order)
+		db.session.commit()
+		return f'Данные заказа "{order.name}" с ID {order.id} успешно обновлены!'
+
+	elif request.method == 'DELETE':
+		db.session.delete(order)
+		db.session.commit()
+		return f'Заказ "{order.name}" с ID {order.id} успешно удален из базы!'
 
 
-@app.route('/offers/')
+@app.route('/offers/', methods=['GET', 'POST'])
 def offers():
-	result = []
-	for offer in db.session.query(Offer).all():
-		result.append({
-			'id': offer.id,
-			'order_id': offer.order_id,
-			'executor_id': offer.executor_id
-		})
-	return jsonify(result)
+	if request.method == 'GET':
+		result = []
+		for offer in db.session.query(Offer).all():
+			result.append({
+				'id': offer.id,
+				'order_id': offer.order_id,
+				'executor_id': offer.executor_id
+			})
+		return jsonify(result)
+
+	elif request.method == 'POST':
+		offer = request.json
+		new_offer = Offer(
+			id=offer.get('id'),
+			order_id=offer.get('order_id'),
+			executor_id=offer.get('executor_id')
+		)
+		with db.session.begin():
+			db.session.add(new_offer)
+		return f'Новое предложение "{new_offer.name}" успешно добавлено в базу!'
 
 
-@app.route('/offers/<int:id>/')
+@app.route('/offers/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def offer_by_id(id):
 	offer = db.session.query(Offer).get(id)
-	return jsonify({
-			'id': offer.id,
-			'order_id': offer.order_id,
-			'executor_id': offer.executor_id
-			})
+
+	if request.method == 'GET':
+		return jsonify({
+				'id': offer.id,
+				'order_id': offer.order_id,
+				'executor_id': offer.executor_id
+				})
+
+	elif request.method == 'PUT':
+		update = request.json
+
+		offer.id = update.get('id', offer.id)
+		offer.order_id = update.get('order_id', offer.order_id)
+		offer.executor_id = update.get('executor_id', offer.executor_id)
+
+		db.session.add(offer)
+		db.session.commit()
+		return f'Данные предложения "{offer.name}" с ID {offer.id} успешно обновлены!'
+
+	elif request.method == 'DELETE':
+		db.session.delete(offer)
+		db.session.commit()
+		return f'Заказ "{offer.name}" с ID {offer.id} успешно удален из базы!'
 
 
 def print_table():
